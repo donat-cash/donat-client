12cde72f18e38ec86f1347bf4fb556517e6e007d
Vadim Yalovenko <yalovek@ya.ru>
2017-06-23 20:53:58 +0300

    wip

--- src/components/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export default ({ component: C, props: cProps, ...rest }) => (
-  <Route {...rest} render={props => (
-    cProps.userToken === null
-      ? <C {...props} {...cProps} />
-      : <Redirect to="/" />
-  )}/>
-);
-
 function querystring(name, url = window.location.href) {
   name = name.replace(/[[]]/g, "\\$&");
 
